e42d9943600d66c4b8ff5bb3a9ef59c334ee6e90
Robert Eggl <robert@eggl.dev>
2026-07-25 21:34:53 +0200

    docs: improve font handling
    Signed-off-by: Robert Eggl <robert@eggl.dev>

--- rspress.config.ts
+++ rspress.config.ts
@@ -67,28 +67,6 @@ const config: UserConfig = {
     html: {
       tags: [
         {
-          tag: "link",
-          attrs: {
-            rel: "preconnect",
-            href: "https://fonts.googleapis.com",
-          },
-        },
-        {
-          tag: "link",
-          attrs: {
-            rel: "preconnect",
-            href: "https://fonts.gstatic.com",
-            crossorigin: true,
-          },
-        },
-        {
-          tag: "link",
-          attrs: {
-            rel: "stylesheet",
-            href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=Instrument+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap",
-          },
-        },
-        {
           tag: "meta",
           attrs: {
             name: "theme-color",
